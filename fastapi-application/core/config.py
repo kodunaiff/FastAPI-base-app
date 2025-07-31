724ce2c0a4198b08eb573c3745844391e83b8d1e
kodunaiff <kodunaiff@gmail.com>
2025-08-01 00:31:03 +0800

    create dbhelper

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
 
 
@@ -11,9 +12,14 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
---
+++ fastapi-application/core/models/db_helper.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine
+)
+
+
+class DatabaseHelper:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        pool_size: int = 5,
+        max_overflow: int = 10,
+    ):
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
