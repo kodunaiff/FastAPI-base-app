21544cd50cf827dfb725e3f25d0727091e1a3607
kodunaiff <kodunaiff@gmail.com>
2025-07-31 22:46:29 +0800

    add port and host in settings

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -17,4 +17,9 @@ def index():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
